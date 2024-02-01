320d56f89cc2e200154742b69badcf31adcdca9d
Virendra108 <113795394+Virendra108@users.noreply.github.com>
2024-02-01 16:47:40 +0530

    Add files via upload

---
+++ adsas1.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+class lms{
+	int book_id,price,i,n;
+	char *title,*auther,*pub,*isbn_no;
+	public:
+	void accept();
+	void display();
+	void search();
+	void update();
+	void Delete();
+}b[100];
+
+void lms::accept(){
+	cout<<"enter the book_id,price,title,author,pub,isbn number :"<<endl;
+	cin>>book_id>>price>>title>>auther>>pub>>isbn_no>>;
+}
+void lms::display(){
+	cout<<"\n"<<book_id<<"\t"<<price<<"\t"<<title<<"\t"<<auther<<"\t"<<pub<<"\t"<<isbn_no<<endl;
+	}
+void lms::search(){
+	int f=0;
+	cout<<"enter book id: ";
+	cin>>book_id;
+	for(i=0;i<n;i++){
+		if(book_id==b[i].book_id){
+			f=1;
+			cout<<"book id found";
+			b[i].display();
+			break;
+		}
+	}
+	if(f==0){
+		cout<<"book id not found";
+	}
+}
+void lms::update(){
+	cout<<"Enter the book id which you want to update: ";
+	cin>>book_id;
+	if(book_id==b[i].book_id){
+		cout<<"enter book info which you want to update:";
+		cout<<"book id";
+		cin>>b[i].price;
+		cout<<"title";
+		cin>>b[i].auther;
+		cout<<"publsher";
+		cin>>b[i].pub;
+	}
+}
+void lms::Delete(){
+	cout<<"Enter the book id :";
+	cin>>book_id;
+	for(i=0;i<n;i++){
+		if(book_id==b[i].book_id){
+			do{
+				b[i]=b[i+1];
+				i+1;
+			}
+			while(i<n);
+			n--;
+			break;
+		}
+	}
+}
+int main(){
+	int choice;
+	do{
+		cout<<"\n1.accept";
+		cout<<"\n2.display";
+		cout<<"\n3.search";
+		cout<<"\n4.update";
+		cout<<"\n5.delete";
+		cout<<"\n6.exit";
+		cout<<"\nenter your choice: "<<endl;
+		cin>>choice;
+		switch
